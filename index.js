de5961699f89f15138f48ef5d48662c80d10e1ab
cee2 <cee2nmnp@gmail.com>
2022-07-09 12:09:46 +0900

    test

---
+++ index.js
@@ -0,0 +1,38 @@
+// インポート
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+// 初期化
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
+
+// webサーバーの設定
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/src/index.html');
+});
+server.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
+
+// ソケット通信の設定
+io.on('connection', (socket) => {
+	console.log('user connected');
+
+	// 初回接続時にクライアントからくる、接続IDの要求に対する処理
+	socket.on('requestId', (message) => {
+		io.emit('responseId', socket.id);
+		console.log('新規接続開始。ソケットID： ', socket.id);
+	});
+
+	// 'sendMessage' というイベント名で受信できる
+	// 第一引数には受信したメッセージが入り、ログに出力する
+	socket.on('sendMessage', (message) => {
+		console.log('Message has been sent: ', message);
+		
+		// 'receiveMessage' というイベントを発火、受信したメッセージを全てのクライアントに対して送信する
+		io.emit('receiveMessage', message);
+	});
+});
